3ba0e18ff52fb623dbc3207d515f0398b6d7f165
soloman <soloman1124@gmail.com>
2014-10-02 12:32:17 +1000

    Minimal implementation

--- lib/beaker.rb
+++ lib/beaker.rb
@@ -1,5 +1,36 @@
 require "beaker/version"
 
 module Beaker
+  extend self
 
+  attr_accessor :base_url
+
+  class Session
+    def initialize user_id
+      @user_id = user_id
+      @base_url = Beaker.base_url
+    end
+
+    def participate experiment_name
+      group = participate_request experiment_name
+      yield group if block_given?
+
+      group
+    end
+
+    private
+
+    attr_reader :user_id, :base_url
+
+    def participate_request experiment_name
+      uri = participate_uri experiment_name
+      res = Net::HTTP.get_response uri
+
+      res.body if res.is_a?(Net::HTTPSuccess)
+    end
+
+    def participate_uri experiment_name
+      URI base_url + "/participate/#{experiment_name}/#{user_id}"
+    end
+  end
 end
